4548d083a4dcc38ff7747820d5584d9d23daffe6
YeldosQoja <yeldos.kozha@gmail.com>
2024-06-30 16:10:02 +0500

    Registered custom user model to admin site & Created custom forms.

--- chess/admin.py
+++ chess/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth import get_user_model
+from .forms import UserChangeForm, UserCreationForm
 
-# Register your models here.
+User = get_user_model()
+
+
+class UserAdmin(BaseUserAdmin):
+    # The forms to add and change user instances
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    list_display = ["email", "username", "first_name", "last_name", "is_admin"]
+    list_filter = ["is_admin", "email", "username"]
+    fieldsets = [
+        (None, {"fields": ["email", "username", "first_name", "last_name"]}),
+        ("Permissions", {"fields": ["is_admin"]}),
+    ]
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = [
+        (
+            None,
+            {
+                "classes": ["wide"],
+                "fields": ["email", "password1", "password2"],
+            },
+        ),
+    ]
+    search_fields = ["email"]
+    ordering = ["email"]
+    filter_horizontal = []
+
+
+admin.site.register(User, UserAdmin)
