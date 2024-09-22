3549852b4ec3a979c5620de5bd9675493a2df201
Yeldos Kozhabay <yeldos.kozha@gmail.com>
2024-09-22 19:14:41 +0500

    Changed database from postgresql to mysql.

--- core/settings.py
+++ core/settings.py
@@ -114,11 +114,12 @@ CHANNEL_LAYERS = {
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": os.environ.get("DB_NAME"),
         "USER": os.environ.get("DB_USER"),
         "PASSWORD": os.environ.get("DB_PASSWORD"),
         "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT"),
     }
 }
 
