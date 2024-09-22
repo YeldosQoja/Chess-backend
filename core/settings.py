72ede909542b7e1d8c238e0a95fa3baa12f9d496
Yeldos Kozhabay <yeldos.kozha@gmail.com>
2024-09-22 20:24:19 +0500

    Changed STATIC_ROOT & Added STATICFILES_DIRS

--- core/settings.py
+++ core/settings.py
@@ -99,12 +99,14 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [{
-                "host": os.environ.get("REDIS_HOST"),
-                "port": os.environ.get("REDIS_PORT"),
-                "username": os.environ.get("REDIS_USERNAME"),
-                "password": os.environ.get("REDIS_PASSWORD"),
-            }]
+            "hosts": [
+                {
+                    "host": os.environ.get("REDIS_HOST"),
+                    "port": os.environ.get("REDIS_PORT"),
+                    "username": os.environ.get("REDIS_USERNAME"),
+                    "password": os.environ.get("REDIS_PASSWORD"),
+                }
+            ]
         },
     }
 }
@@ -160,7 +162,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
