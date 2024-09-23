8da927a6020916e21651318bb1b64f45db3db04b
Yeldos Kozhabay <yeldos.kozha@gmail.com>
2024-09-23 14:26:08 +0500

    Added connection to Heroku Postgres.

--- core/settings.py
+++ core/settings.py
@@ -182,3 +182,6 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     }
 }
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
